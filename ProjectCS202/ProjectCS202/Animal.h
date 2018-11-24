9ac194c78e13c7741eac1f7c1593fef750c0d761
Another Team <tbphuc@apcs.vn>
2018-11-24 14:17:29 +0700

    edit Animal.h

--- ProjectCS202/ProjectCS202/Animal.h
+++ ProjectCS202/ProjectCS202/Animal.h
@@ -1,10 +1,73 @@
 #include "Object.h"
 class CANIMAL :public CObject {
+public:
+	CANIMAL(int up, int down, int left, int right) :CObject(up, down, left, right) {
 
+	}
+	CANIMAL() = default;
+	~CANIMAL();
+	virtual bool isCollide(const CPEOPLE &tmp)
+	{
+		return CObject::isCollide(tmp);
+	}
+	virtual void GoRight()
+	{
+		CObject::GoRight();
+	}
+	virtual void GoLeft()
+	{
+		CObject::GoLeft();
+	}
+	int getSize() {
+		return Num;
+	}
+private:
+	int Num;//numberofanimals
 };
-class CDINAUSOR :public CANIMAL {
 
+
+class CDINOSAUR :public CANIMAL {
+public:
+	CDINOSAUR(int up, int down, int left, int right) :CANIMAL(up, down, left, right) {
+
+	}
+	CDINOSAUR() = default;
+	~CDINOSAUR();
+	bool isCollide(const CPEOPLE &tmp)
+	{
+		return CANIMAL::isCollide(tmp);
+	}
+	void GoRight()
+	{
+		CANIMAL::GoRight();
+	}
+	void GoLeft()
+	{
+		CANIMAL::GoLeft();
+	}
+private:
+	void DDINOSAUR();//drawing DINOSAUR
 };
+
 class CBIRD :public CANIMAL {
+public:
+	CBIRD(int up, int down, int left, int right) :CANIMAL(up, down, left, right) {
 
+	}
+	CBIRD() = default;
+	~CBIRD();
+	bool isCollide(const CPEOPLE &tmp)
+	{
+		return CANIMAL::isCollide(tmp);
+	}
+	void GoRight()
+	{
+		CANIMAL::GoRight();
+	}
+	void GoLeft()
+	{
+		CANIMAL::GoLeft();
+	}
+private:
+	void DBIRD();//drawing BIRD
 };
