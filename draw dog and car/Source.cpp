2915d6bfadc63c1a6ad12b0cff7a5c24c4784f1b
vanluong1811 <45174764+vanluong1811@users.noreply.github.com>
2018-11-30 23:54:05 +0700

    Add files via upload

--- draw dog and car/Source.cpp
+++ draw dog and car/Source.cpp
@@ -6,6 +6,7 @@ void gotoxy(int x, int y);
 void drawdog(int x, int y);
 void FixConsoleWindow();
 void drawcar(int x, int y);
+void drawplayer(int x, int y);
 
 void drawcar(int x, int y)
 {
@@ -136,22 +137,87 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void drawplayer(int x, int y)
+{
+	char a = 219, b = 220, c = 223, d = 221, e = 222;
+	gotoxy(x, y);
+	//cao 4 dai 4
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	cout << d;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	gotoxy(x+1, y); cout << a;
+	gotoxy(x+2, y); cout << a;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	gotoxy(x+3, y); cout << e;
+	gotoxy(x+1, y-1); cout << a;
+	gotoxy(x+2, y-1); cout << a;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	gotoxy(x+1, y+1); cout << d;
+	gotoxy(x+2, y+1); cout << e;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+	gotoxy(x+1, y-2); cout << b;
+	gotoxy(x+2, y-2); cout << b;
+}
+
 int main()
 {
 	FixConsoleWindow();
 	HWND console = GetConsoleWindow();
 	HDC mydc = GetDC(console);
-	COLORREF color = RGB(255, 255, 255);
+	//COLORREF color = RGB(255, 255, 255);
 	RECT r;
 	GetWindowRect(console, &r); //stores the console's current dimensions
 
 	MoveWindow(console, r.left, r.top, 1920, 1080, TRUE); // 800 width, 100 height
-		
-	drawdog(1, 20);
+	
+	/*SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+	gotoxy(50, 20);
+	char a = 219, b = 220,c=223;
+	cout << a;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	for (int i = 20; i < 23; ++i)
+	{
+		gotoxy(51, i);
+		cout << a;
+	}
+	gotoxy(52, 20);
+	cout << b;
+	gotoxy(52, 21);
+	cout << a;
+	gotoxy(52, 22);
+	cout << c;
+	gotoxy(53, 20); cout << b;
+	gotoxy(53, 21); cout << a;
+	gotoxy(54, 20); cout << b;
+	gotoxy(54, 21); cout << a;
+	gotoxy(55, 20); cout << a;
+	gotoxy(55, 21); cout << a;
+	gotoxy(56, 20); cout << a;
+	gotoxy(56, 21); cout << a;
+	gotoxy(56, 19); cout << b;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+	gotoxy(57, 19); cout << a;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	for (int i = 20; i < 22; ++i)
+	{
+		gotoxy(57, i); cout << a;
+	}
+	gotoxy(57, 22); cout << c;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	gotoxy(58, 19); cout << a;
+	gotoxy(58, 20); cout << b;
+	gotoxy(58, 21); cout << a;
+	gotoxy(58, 22); cout << a;
+	gotoxy(59, 19); cout << a;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+	gotoxy(59, 20); cout << a;*/
+	
+	//drawdog(1, 20);
 	//drawdog(50, 20);
 
-	drawcar(50, 20);
+	//drawcar(50, 20);
 
+	drawplayer(50, 20);
 	
 	cout << endl;
 	system("pause>nil");
